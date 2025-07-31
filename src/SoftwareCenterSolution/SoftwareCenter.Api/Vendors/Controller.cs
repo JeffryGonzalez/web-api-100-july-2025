e8f928228f270eb6fe104e12be40cc94054dd063
Ryan Hurley <ryan.hurley1212@gmail.com>
2025-07-31 21:12:24 +0000

    Migrated Model definition for Vendors to it's own class

--- src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/Controller.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/Controller.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using FluentValidation;
+﻿using FluentValidation;
 using Marten;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -110,54 +109,3 @@ public class Controller(IDocumentSession session) : ControllerBase
     }
 
 }
-
-/*{
-    "name": "Microsoft",
-    "url": "https://wwww.microsoft.com",
-    "pointOfContact": {
-        "name": "Satya",
-        "phone": "800 big-boss",
-        "email": "satya@microsoft.com"
-    }
-}*/
-
-public record CreateVendorRequest
-{
-
-    public string Name { get; init; } = string.Empty;
-    public string Url { get; init; } = string.Empty;
-    public CreateVendorPointOfContactRequest PointOfContact { get; init; } = new();
-}
-
-public class CreateVendorRequestValidator : AbstractValidator<CreateVendorRequest>
-{
-    public CreateVendorRequestValidator()
-    {
-        RuleFor(v => v.Name).NotEmpty().MinimumLength(3).MaximumLength(255);
-        RuleFor(v => v.Url).NotEmpty();
-        RuleFor(v => v.PointOfContact).NotNull();
-    }
-}
-
-public record CreateVendorPointOfContactRequest
-{
-    public string Name { get; init; } = string.Empty;
-    public string Phone { get; init; } = string.Empty;
-    public string Email { get; init; } = string.Empty;
-};
-
-public class CreateVendorPointOfContactRequestValidator :
-    AbstractValidator<CreateVendorPointOfContactRequest>
-{
-    public CreateVendorPointOfContactRequestValidator()
-    {
-        RuleFor(p => p.Name).NotEmpty();
-
-    }
-}
-
-public record CreateVendorResponse(
-    Guid Id,
-    string AddedBy,
-    string Name, string Url, CreateVendorPointOfContactRequest PointOfContact
-    );
